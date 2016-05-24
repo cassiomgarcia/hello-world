0b4d554204895f14d868ba35bf7b8a00a9cf1d78
cassiomgarcia <cassiomg@yahoo.com>
2016-05-24 15:55:12 -0300

    Delete run.bat

--- ProjetoCisao/01.controle/Certificados NFe/run.bat
+++
@@ -1,4 +0,0 @@
-rem java -Djavax.net.ssl.keyStore=C:/Temp/certificado/alphaville.ks -Djavax.net.ssl.trustStorePassword=ti_alphaville -Djavax.net.debug=all Main
-rem java -Djavax.net.ssl.trustKeyStore=C:/Temp/certificado/NFeCacerts -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.keyStore=C:/Temp/certificado/alphaville.ks -Djavax.net.ssl.keyStorePassword=ti_alphaville -Djavax.net.debug=all Main
-rem java -Djavax.net.debug=all Main
-java Main
